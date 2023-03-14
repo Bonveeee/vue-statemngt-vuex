33295aac3e8a0b741649aad3eaf778e8ec8df795
bonveeee <bonochieng@gmail.com>
2023-03-14 06:12:22 +0300

    modified components

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,22 +1,51 @@
 <template>
-  <h3>Make it or leave it</h3>
+  <h3>Work Flow</h3>
     <div class="todos">
       <div v-for="todo in allTodos" :key="todo.id" class="todo">
           {{ todo.title }}
+          <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
         </div>
     </div>
 </template>
 
 <script>
 // connects getter from vuex to this component
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 export default {
     name: "Todos",
+    methods: {
+    ...mapActions(["fetchTodos", "deleteTodo", "updateTodo"]),
      //define getters, on which we wanna use
-     computed: mapGetters(['allTodos'])
+     computed: mapGetters(['allTodos']),
+     created() {
+    this.fetchTodos();
+  }
+}
 }
 </script>
 
 <style scoped>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
+  cursor: pointer;
+}
 
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -12,5 +12,5 @@ Vue.use(Vuex);
 export default new Vuex.Store({
 modules: {
     todos
-},
-})
+}
+});
